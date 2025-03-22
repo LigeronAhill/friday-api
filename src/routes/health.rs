b89aa5065afeac8040df973af1e3173d3f464cf2
Aleksandr Provotorov <provotorov@inbox.ru>
2025-03-22 11:10:04 +0300

    health

--- src/routes/health.rs
+++ src/routes/health.rs
@@ -2,5 +2,5 @@ use axum::response::IntoResponse;
 use http::StatusCode;
 
 pub async fn health_check() -> impl IntoResponse {
-    (StatusCode::OK, "My health is fine").into_response()
+    (StatusCode::OK, "My health is fine, thanks").into_response()
 }
